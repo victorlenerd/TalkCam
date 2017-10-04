9e583fd4b13ce319eb9725e6c7e22e624299850c
sleekvick <nvonweb@outlook.com>
2017-10-04 02:16:01 +0100

    Add camera component

--- root.js
+++ root.js
@@ -7,6 +7,8 @@ import {
     TouchableOpacity
 } from 'react-native';
 
+import { CameraKitCamera } from 'react-native-camera-kit';
+
 export default class Root extends Component {
     state = {
         ready: false,
@@ -15,7 +17,21 @@ export default class Root extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.camSpace}></View>
+                <View style={styles.camSpace}>
+                    <CameraKitCamera
+                        ref={cam => this.camera = cam}
+                        style={{
+                            flex: 1,
+                            backgroundColor: '#000000'
+                        }}
+                        cameraOptions={{
+                            flashMode: 'auto',             // on/off/auto(default)
+                            focusMode: 'on',               // off/on(default)
+                            zoomMode: 'on',                // off/on(default)
+                            ratioOverlay:'1:1',            // optional, ratio overlay on the camera and crop the image seamlessly
+                            ratioOverlayColor: '#00000077' // optional
+                        }} />
+                </View>
                 <TouchableOpacity style={[styles.talkButton, (this.state.ready) ? { opacity: 1 } : { opacity: 0.2 } ]}>
                     <Text style={styles.talkText}>Start Talking</Text>
                 </TouchableOpacity>
