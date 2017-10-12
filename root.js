9d8648b6edbf82d6f4a8a6772ed2e7c0d17be153
sleekvick <nvonweb@outlook.com>
2017-10-12 02:39:30 +0100

    Capture with camera and convert image to base64 uri

--- root.js
+++ root.js
@@ -4,37 +4,63 @@ import {
     StyleSheet,
     Text,
     View,
+    Image,
+    Dimensions,
     TouchableOpacity
 } from 'react-native';
 
 import { CameraKitCamera } from 'react-native-camera-kit';
+import fs from 'react-native-fs';
+
+const { width, height } = Dimensions.get('window');
 
 export default class Root extends Component {
     state = {
         ready: false,
+        imageUri: ''
+    }
+
+    snap = async () => {
+        const image = await this.camera.capture();
+        const imageUri = await fs.readFile(image.uri, 'base64');
+        this.setState({ ready: true, imageUri });
+    }
+
+    renderCamera = () => {
+        if (this.state.ready) {
+            return (
+                <Image style={{flex: 1, backgroundColor: '#0E86FA'}} resizeMode='contain' source={{ uri: `data:image/jpg;base64,${this.state.imageUri}` }} />
+            );
+        }
+
+        return (
+            <CameraKitCamera
+                ref={cam => this.camera = cam}
+                style={{
+                    flex: 1,
+                    backgroundColor: '#000000'
+                }}
+                cameraOptions={{
+                    flashMode: 'auto',             // on/off/auto(default)
+                    focusMode: 'on',               // off/on(default)
+                    zoomMode: 'on',                // off/on(default)
+                    ratioOverlayColor: '#0E86FA' // optional
+                }}
+            />
+        );
     }
 
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.camSpace}>
-                    <CameraKitCamera
-                        ref={cam => this.camera = cam}
-                        style={{
-                            flex: 1,
-                            backgroundColor: '#000000'
-                        }}
-                        cameraOptions={{
-                            flashMode: 'auto',             // on/off/auto(default)
-                            focusMode: 'on',               // off/on(default)
-                            zoomMode: 'on',                // off/on(default)
-                            ratioOverlay:'1:1',            // optional, ratio overlay on the camera and crop the image seamlessly
-                            ratioOverlayColor: '#00000077' // optional
-                        }} />
+                    {this.renderCamera()}
                 </View>
-                <TouchableOpacity style={[styles.talkButton, (this.state.ready) ? { opacity: 1 } : { opacity: 0.2 } ]}>
-                    <Text style={styles.talkText}>Start Talking</Text>
-                </TouchableOpacity>
+                <View style={styles.controlSpaces}>
+                    <TouchableOpacity onPress={this.snap} style={[styles.talkButton]}>
+                        <View style={styles.innerCircle}></View>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
     }
@@ -48,8 +74,10 @@ const styles = StyleSheet.create({
         flex: 1
     },
     talkButton: {
-        height: 100,
-        backgroundColor: '#336E7B',
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        backgroundColor: '#FFC221',
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
@@ -57,5 +85,20 @@ const styles = StyleSheet.create({
     talkText: {
         color: '#ffffff',
         fontSize: 20
+    },
+    innerCircle: {
+      width: 70,
+      height: 70,
+      borderRadius: 35,
+      borderWidth: 1,
+      borderColor: '#0E86FA'  
+    },
+    controlSpaces: {
+        width, 
+        height: 100, 
+        backgroundColor: '#0E86FA',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 });
